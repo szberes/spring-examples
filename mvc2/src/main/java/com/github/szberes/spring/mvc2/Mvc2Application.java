c01b69f7af33ff24ae87d73e497648e42ea331a9
Szabolcs Beres <szabolcsb1@gmail.com>
2018-03-29 06:35:41 +0200

    Add project for serving static resources with spring boot

---
+++ mvc2/src/main/java/com/github/szberes/spring/mvc2/Mvc2Application.java
@@ -0,0 +1,12 @@
+package com.github.szberes.spring.mvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc2Application.class, args);
+	}
+}
