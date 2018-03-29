0fc48956c6f05967866e4baee259a0be8ac302e3
Szabolcs Beres <szabolcsb1@gmail.com>
2018-03-29 07:04:15 +0200

    Configure WebMvc to serve resources from different folders

---
+++ mvc-static/src/main/java/com/github/szberes/spring/mvc2/StaticResourcesConfiguration.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 Szabolcs Balazs Beres.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.github.szberes.spring.mvc2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourcesConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations("file:/path/to/my/dropbox/");
+        // Static is accessible by default
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/", "classpath:/static2/");
+    }
+}
